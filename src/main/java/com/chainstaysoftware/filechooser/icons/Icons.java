fd4bea76f981abe47016f488106fa5351703dfdf
Ric Emery <ric.emery@gmail.com>
2015-11-13 22:02:14 -0700

    Add additional filetype icons

--- src/main/java/com/chainstaysoftware/filechooser/icons/Icons.java
+++ src/main/java/com/chainstaysoftware/filechooser/icons/Icons.java
@@ -1,9 +1,13 @@
 package com.chainstaysoftware.filechooser.icons;
 
 import javafx.scene.image.Image;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class Icons {
    public static final int SMALL_ICON_WIDTH = 25;
@@ -15,10 +19,27 @@ public class Icons {
    public static final String LIST_WITH_PREVIEW_VIEW_24 = "/icons8/24x24/Columns-24.png";
 
    public static final String FOLDER_64 = "/icons8/64x64/Folder-64.png";
-   public static final String GENERIC_FILE_64 = "/icons8/64x64/File-64.png";
    public static final String HARDDISK_64 = "/icons8/64x64/HDD-64.png";
    public static final String OPEN_FOLDER_64 = "/icons8/64x64/Open Folder-64.png";
    public static final String USER_HOME_64 = "/icons8/64x64/User Folder-64.png";
+
+   public static final String GENERIC_FILE_64 = "/icons8/64x64/File-64.png";
+   public static final String JPG_64 = "/icons8/64x64/JPG-64.png";
+   public static final String PDF_64 = "/icons8/64x64/PDF-64.png";
+   public static final String PNG_64 = "/icons8/64x64/PNG-64.png";
+   public static final String PS_64 = "/icons8/64x64/PS-64.png";
+   public static final String TXT_64 = "/icons8/64x64/TXT-64.png";
+   public static final String XML_64 = "/icons8/64x64/XML-64.png";
+
+   private static final Map<String, String> fileTypeIcons = new HashMap<>();
+   static {
+      fileTypeIcons.put("jpg", JPG_64);
+      fileTypeIcons.put("pdf", PDF_64);
+      fileTypeIcons.put("png", PNG_64);
+      fileTypeIcons.put("ps", PS_64);
+      fileTypeIcons.put("txt", TXT_64);
+      fileTypeIcons.put("xml", XML_64);
+   }
 
    /**
     * Load an icon from the CURSOR_PATH location in the class path.
@@ -42,9 +63,17 @@ public class Icons {
 
    public Image getIconForFile(final File file) {
       // TODO: Optimize
-      return file.isDirectory()
-            ? getIcon(Icons.FOLDER_64)
-            : getIcon(Icons.GENERIC_FILE_64);
+      if (file.isDirectory()) {
+         return getIcon(Icons.FOLDER_64);
+      }
+
+      final String extension = FilenameUtils.getExtension(file.getName()).toLowerCase(Locale.ENGLISH);
+      final String iconFile = fileTypeIcons.get(extension);
+      if (iconFile == null) {
+         return getIcon(Icons.GENERIC_FILE_64);
+      }
+
+      return getIcon(iconFile);
    }
 }
 
