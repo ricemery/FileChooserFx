43c1b033512ba081ab971522f9570c97ab7f05f3
Ric Emery <ric.emery@gmail.com>
2016-02-03 22:07:35 -0700

    Add OsxPlacesProvider

--- src/main/java/com/chainstaysoftware/filechooser/os/Places.java
+++ src/main/java/com/chainstaysoftware/filechooser/os/Places.java
@@ -2,6 +2,7 @@ package com.chainstaysoftware.filechooser.os;
 
 import com.chainstaysoftware.filechooser.os.def.DefaultPlacesProvider;
 import com.chainstaysoftware.filechooser.os.linux.LinuxPlacesProvider;
+import com.chainstaysoftware.filechooser.os.osx.OsxPlacesProvider;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public class Places implements PlacesProvider {
    private final PlacesProvider defaultPlacesProvider = new DefaultPlacesProvider();
    private final PlacesProvider linuxPlacesProvider = new LinuxPlacesProvider();
+   private final PlacesProvider osxPlacesProvider = new OsxPlacesProvider();
 
    /**
     * Get the default list of {@link Place} to show in the Places list.
@@ -19,6 +21,10 @@ public class Places implements PlacesProvider {
    public List<Place> getDefaultPlaces() {
       if (OsInfo.isLinux()) {
          return linuxPlacesProvider.getDefaultPlaces();
+      }
+
+      if (OsInfo.isMac()) {
+         return osxPlacesProvider.getDefaultPlaces();
       }
 
       return defaultPlacesProvider.getDefaultPlaces();
---
+++ src/main/java/com/chainstaysoftware/filechooser/os/osx/OsxPlacesProvider.java
@@ -0,0 +1,65 @@
+package com.chainstaysoftware.filechooser.os.osx;
+
+import com.chainstaysoftware.filechooser.os.Place;
+import com.chainstaysoftware.filechooser.os.PlaceType;
+import com.chainstaysoftware.filechooser.os.PlacesProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * {@link PlacesProvider} for Linux. NOTE - This may not be the best way to
+ * get this info from Linux. Java does not make it easy..
+ */
+public class OsxPlacesProvider implements PlacesProvider {
+   private static Logger logger = Logger.getLogger("com.chainstaysoftware.filechooser.os.osx.OsxPlacesProvider");
+
+   /**
+    * Return the default list of {@link Place} for the supporting OS instance.
+    */
+   @Override
+   public List<Place> getDefaultPlaces() {
+      final List<Place> places = new LinkedList<>();
+
+      final File volumes = new File("/Volumes");
+      final String[] children = volumes.list();
+
+      Arrays.stream(children)
+            .forEach(s -> {
+               try {
+                  File path = new File(volumes, s);
+                  if (Files.isSymbolicLink(path.toPath())) {
+                     path = Files.readSymbolicLink(path.toPath()).toFile();
+                  }
+
+                  places.add(new Place(toPlaceType(path), path));
+               } catch (IOException e) {
+                  logger.log(Level.WARNING, "Error finding OS X mounts", e);
+               }
+            });
+
+      return places;
+   }
+
+   private PlaceType toPlaceType(final File path)
+         throws IOException {
+      final String fsType = Files.getFileStore(path.toPath()).type();
+      if ("cd9660".equals(fsType)) {
+         return PlaceType.Cd;
+      }
+
+      // Code is assuming that "msdos" is a Usb drive.. This probably
+      // isn't 100% correct. Haven't been able to find a better solution.
+      if ("msdos".equals(fsType)) {
+         return PlaceType.Usb;
+      }
+
+      return PlaceType.HardDisk;
+   }
+}
