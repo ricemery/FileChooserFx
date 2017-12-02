cde94c857b2b899d29e49b87420dc6669166a3a0
jackrabbit128 <uCU7BNZL>
2017-12-02 22:46:07 +0100

    Use a JavaFX Service for watching current directory and simplify watching.

--- src/main/java/com/chainstaysoftware/filechooser/DirectoryWatcherTask.java
+++ src/main/java/com/chainstaysoftware/filechooser/DirectoryWatcherTask.java
@@ -2,67 +2,63 @@ package com.chainstaysoftware.filechooser;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
-import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Task to watch for file system changes in the currentDirectory.
+ * Task to watch for file system changes.
  */
-class DirectoryWatcherTask extends Task {
+class DirectoryWatcherTask extends Task<Void> {
    private static Logger logger = Logger.getLogger("com.chainstaysoftware.filechooser.DirectoryWatcherTask");
 
-   private final Stage owner;
-   private final WatchService watcher;
+   private final File directory;
    private final FilesViewCallback callback;
 
-   public DirectoryWatcherTask(final Stage owner,
-                               final WatchService watcher,
+   public DirectoryWatcherTask(final File directory,
                                final FilesViewCallback callback) {
-      this.owner = owner;
-      this.watcher = watcher;
+      this.directory = directory;
       this.callback = callback;
    }
 
    @Override
-   protected Object call() throws Exception {
-      while (true) {
-         WatchKey key;
-         try {
-            final long timeout = 5;
-            key = watcher.poll(timeout, TimeUnit.SECONDS);
-         } catch (InterruptedException ex) {
-            return null;
-         }
+   protected Void call() {
+      try (FileSystem fileSystem = directory.toPath().getFileSystem();
+           WatchService watcher = fileSystem.newWatchService()) {
+         directory.toPath().register(watcher,
+                 StandardWatchEventKinds.ENTRY_CREATE,
+                 StandardWatchEventKinds.ENTRY_DELETE,
+                 StandardWatchEventKinds.ENTRY_MODIFY);
+         while (!isCancelled()) {
+            WatchKey key = watcher.take();
 
-         if (shouldShutdownTask()) {
-            break;
-         }
+            if (key.pollEvents().isEmpty()) {
+               continue;
+            }
 
-         if (key == null || key.pollEvents().isEmpty()) {
-            continue;
-         }
+            logger.log(Level.FINE, "Directory contents changed. Updating view.");
+            Platform.runLater(callback::updateFiles);
 
-         logger.log(Level.FINE, "Directory contents changed. Updating view.");
-         Platform.runLater(callback::updateFiles);
-
-         boolean valid = key.reset();
-         if (!valid) {
-            break;
+            boolean valid = key.reset();
+            if (!valid) {
+               break;
+            }
          }
+      } catch (InterruptedException e) {
+         if (!isCancelled()) {
+            logger.log(Level.FINE, "interrupted while waiting for key: " + e.getMessage());
+         }
+      } catch (IOException e) {
+         logger.log(Level.FINE, "watching " + directory + " failed: " + e.getMessage());
       }
 
       logger.log(Level.FINE, "exiting watcher task");
       return null;
    }
-
-   private Boolean shouldShutdownTask() {
-      return (Boolean)owner.getUserData();
-   }
 }
-
-
